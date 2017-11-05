4a5d3bb50ff2d50f39e172eb4bdc6bc01fea0348
Ogbuanya Austine <diribe3000@gmail.com>
2017-11-05 06:34:54 -0500

    Added postgres database and sequelize

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { makeExecutableSchema } from 'graphql-tools';
 
 import typeDefs from './schema';
 import resolvers from './resolvers';
+import models from './models';
 
 export const schema = makeExecutableSchema({
     typeDefs,
